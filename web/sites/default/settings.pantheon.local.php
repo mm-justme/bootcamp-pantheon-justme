42b4152f091d7d46677eb92cab875eef3254e1c7
mm-justme <d.kurdelchuk@dev-branch.com>
2025-08-19 11:23:08 +0300

    Add Pantheon local settings

--- web/sites/default/settings.pantheon.local.php
+++ web/sites/default/settings.pantheon.local.php
@@ -2,5 +2,5 @@
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
   $settings['file_public_path'] = 'sites/default/files';
   $settings['file_private_path'] = 'private';
-  $settings['file_temp_path']   = 'tmp';
+  $settings['file_temp_path'] = $_ENV['HOME'] . '/tmp';
 }
