6c208b6ddf648ac18d88bf770bdca0ebb6f2407c
mm-justme <d.kurdelchuk@dev-branch.com>
2025-08-19 11:31:46 +0300

    Use absolute paths for private and tmp on Pantheon

--- web/sites/default/settings.pantheon.local.php
+++ web/sites/default/settings.pantheon.local.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-  $settings['file_public_path'] = 'sites/default/files';
-  $settings['file_private_path'] = 'private';
-  $settings['file_temp_path'] = $_ENV['HOME'] . '/tmp';
+$settings['file_public_path']  = 'sites/default/files';
+$settings['file_private_path'] = $_ENV['HOME'] . '/files/private';
+$settings['file_temp_path']    = $_ENV['HOME'] . '/tmp';
 }
