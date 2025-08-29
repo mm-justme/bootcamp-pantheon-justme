34336ff335b6dc5302ab3564ecadf73a11751ed1
mm-justme <d.kurdelchuk@dev-branch.com>
2025-08-29 10:15:30 +0300

    refs #9613: Installed tailwind - To avoid version-related errors gulp-postcss, postcss were updated to 9 and 8 accordingly

---
+++ web/themes/custom/home_designer_theme/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "templates/**/*.html.twig",
+      "/js/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
