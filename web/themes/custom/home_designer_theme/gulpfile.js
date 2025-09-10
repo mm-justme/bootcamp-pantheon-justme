f8ad4924d84c9cdda98c0d21b5f453b0b23e4189
mm-justme <d.kurdelchuk@dev-branch.com>
2025-09-10 14:55:43 +0300

    refs codingStandards: php and js coding standards were integrated

--- web/themes/custom/home_designer_theme/gulpfile.js
+++ web/themes/custom/home_designer_theme/gulpfile.js
@@ -1,22 +1,23 @@
-const gulp = require('gulp');
-const dartSass = require('sass');
-const gulpSass = require('gulp-sass')(dartSass);
+const gulp = require("gulp");
+const dartSass = require("sass");
+const gulpSass = require("gulp-sass")(dartSass);
 
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
-function styles(){
-  return gulp.src('scss/style.scss',{sourcemaps:true})
-    .pipe(gulpSass({outputStyle:'expanded'}))
-    .on('error', gulpSass.logError)
+function styles() {
+  return gulp
+    .src("scss/style.scss", { sourcemaps: true })
+    .pipe(gulpSass({ outputStyle: "expanded" }))
+    .on("error", gulpSass.logError)
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('css', {sourcemaps:'.'}));
+    .pipe(gulp.dest("css", { sourcemaps: "." }));
 }
 
-const watch = gulp.series(styles,function startWatch(){
-  gulp.watch('scss/**/*.scss', styles);
+const watch = gulp.series(styles, function startWatch() {
+  gulp.watch("scss/**/*.scss", styles);
 });
 
-exports.styles = styles
-exports.watch = watch
-exports.dev = watch
+exports.styles = styles;
+exports.watch = watch;
+exports.dev = watch;
