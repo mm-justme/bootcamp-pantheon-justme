095c7d30622bafb3edffdaad37aa13a63c687ccc
mm-justme <d.kurdelchuk@dev-branch.com>
2025-09-17 10:12:12 +0300

    refs #9661: Added behavior, styling - added styling including mobile one - added behavior (image opacity)

--- web/themes/custom/home_designer_theme/js/paragraphs/about.js
+++ web/themes/custom/home_designer_theme/js/paragraphs/about.js
@@ -1,15 +1,26 @@
-(function (Drupal, once) {
+(function (Drupal) {
   'use strict';
 
-  Drupal.behaviors.hoverChangeBackground = {
+  Drupal.behaviors.changeElementOpacity = {
     attach: function (context) {
-      console.log("Test start");
-      once('hoverChangeBackground', '.wrapper', context).forEach(function (element) {
-        console.log("Test start ONCE");
+      once('changeElementOpacity', '.changeOpacity', context).forEach(function (element) {
+        const target = document.querySelector('.changeOpacity');
+
+        const observer = new IntersectionObserver((entries, obs) => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                target.style.opacity = 1;
+                obs.unobserve(entry.target);
+              }
+            });
+          },
+          {
+            rootMargin: '0px 0px -60% 0px',
+          },
+        );
+        observer.observe(target);
 
       });
-      console.log("Test end");
-    }
+    },
   };
-
-})(Drupal, once);
+})(Drupal);
