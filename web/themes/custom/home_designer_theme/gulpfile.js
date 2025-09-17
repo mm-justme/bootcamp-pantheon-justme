98a3527d94e0e4a0079c76b26f09a86b57638bf0
mm-justme <d.kurdelchuk@dev-branch.com>
2025-09-17 12:29:41 +0300

    refs #9661: Coding standards only were updated. - installed necessary package in web/ for js eslint - deleted unused package-lock.json in root

--- web/themes/custom/home_designer_theme/gulpfile.js
+++ web/themes/custom/home_designer_theme/gulpfile.js
@@ -1,38 +1,36 @@
-const gulp = require('gulp');
-const dartSass = require('sass');
-const gulpSass = require('gulp-sass')(dartSass);
-const tailwindcss = require('tailwindcss');
+const gulp = require("gulp");
+const dartSass = require("sass");
+const gulpSass = require("gulp-sass")(dartSass);
+const tailwindcss = require("tailwindcss");
 
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
 
 const STYLE_SRC = [
-  'scss/style.scss',
-  'scss/paragraphs/**/*.scss',
-  '!scss/**/_*.scss',
+  "scss/style.scss",
+  "scss/paragraphs/**/*.scss",
+  "!scss/**/_*.scss"
 ];
 
 function styles() {
   return gulp
-    .src(STYLE_SRC, { sourcemaps: true, base: 'scss' })
-    .pipe(gulpSass({ outputStyle: 'expanded' })
-      .on('error', gulpSass.logError))
+    .src(STYLE_SRC, { sourcemaps: true, base: "scss" })
+    .pipe(gulpSass({ outputStyle: "expanded" }).on("error", gulpSass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('css', { sourcemaps: '.' }));
+    .pipe(gulp.dest("css", { sourcemaps: "." }));
 }
 
 function tailwind() {
   return gulp
-    .src('scss/tw.scss', { sourcemaps: true })
-    .pipe(gulpSass({ outputStyle: 'expanded' })
-      .on('error', gulpSass.logError))
+    .src("scss/tw.scss", { sourcemaps: true })
+    .pipe(gulpSass({ outputStyle: "expanded" }).on("error", gulpSass.logError))
     .pipe(postcss([tailwindcss(), autoprefixer()]))
-    .pipe(gulp.dest('css', { sourcemaps: '.' }));
+    .pipe(gulp.dest("css", { sourcemaps: "." }));
 }
 
 const watch = gulp.series(styles, tailwind, function startWatch() {
-  gulp.watch('scss/**/*.scss', gulp.series(styles, tailwind));
-  gulp.watch('tailwind.config.js', tailwind);
+  gulp.watch("scss/**/*.scss", gulp.series(styles, tailwind));
+  gulp.watch("tailwind.config.js", tailwind);
 });
 
 exports.tailwind = tailwind;
--- web/themes/custom/home_designer_theme/js/paragraphs/about.js
+++ web/themes/custom/home_designer_theme/js/paragraphs/about.js
@@ -1,11 +1,9 @@
-(function (Drupal) {
-  'use strict';
-
+(function(Drupal) {
   Drupal.behaviors.changeImgOpacity = {
-    attach: function (context) {
-      once('changeImgOpacity', '.is-visible', context).forEach((el) => {
-
-        const observer = new IntersectionObserver((entries, obs) => {
+    attach(context) {
+      once("changeImgOpacity", ".is-visible", context).forEach(el => {
+        const observer = new IntersectionObserver(
+          (entries, obs) => {
             entries.forEach(entry => {
               if (entry.isIntersecting) {
                 el.style.opacity = 1;
@@ -15,11 +13,11 @@
             });
           },
           {
-            rootMargin: '0px 0px -60% 0px',
-          },
+            rootMargin: "0px 0px -60% 0px"
+          }
         );
         observer.observe(el);
       });
-    },
+    }
   };
 })(Drupal);
