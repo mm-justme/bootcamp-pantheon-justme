25a29e94aeeae23c4cce0949b828c9b71e8fb838
mm-justme <d.kurdelchuk@dev-branch.com>
2025-09-17 10:14:59 +0300

    refs #9661: Updated the package.json and the gulpfile.js - gulpfile.js - added new entry for scss/paragraphs files, added missing info tailwind - added tailwind to the package.json

--- web/themes/custom/home_designer_theme/gulpfile.js
+++ web/themes/custom/home_designer_theme/gulpfile.js
@@ -1,29 +1,41 @@
-const gulp = require("gulp");
-const dartSass = require("sass");
-const gulpSass = require("gulp-sass")(dartSass);
+const gulp = require('gulp');
+const dartSass = require('sass');
+const gulpSass = require('gulp-sass')(dartSass);
+const tailwindcss = require('tailwindcss');
 
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+const STYLE_SRC = [
+  'scss/style.scss',
+  'scss/paragraphs/**/*.scss',
+  '!scss/**/_*.scss',
+];
 
 function styles() {
-  const STYLE_SRC = [
-    'scss/style.scss',
-    'scss/paragraphs/**/*.scss',
-    '!scss/**/_*.scss'
-  ];
-
   return gulp
     .src(STYLE_SRC, { sourcemaps: true, base: 'scss' })
-    .pipe(gulpSass({ outputStyle: "expanded" })
-      .on("error", gulpSass.logError))
+    .pipe(gulpSass({ outputStyle: 'expanded' })
+      .on('error', gulpSass.logError))
     .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest("css", { sourcemaps: "." }));
+    .pipe(gulp.dest('css', { sourcemaps: '.' }));
 }
 
-const watch = gulp.series(styles, function startWatch() {
-  gulp.watch("scss/**/*.scss", styles);
+function tailwind() {
+  return gulp
+    .src('scss/tw.scss', { sourcemaps: true })
+    .pipe(gulpSass({ outputStyle: 'expanded' })
+      .on('error', gulpSass.logError))
+    .pipe(postcss([tailwindcss(), autoprefixer()]))
+    .pipe(gulp.dest('css', { sourcemaps: '.' }));
+}
+
+const watch = gulp.series(styles, tailwind, function startWatch() {
+  gulp.watch('scss/**/*.scss', gulp.series(styles, tailwind));
+  gulp.watch('tailwind.config.js', tailwind);
 });
 
+exports.tailwind = tailwind;
 exports.styles = styles;
 exports.watch = watch;
 exports.dev = watch;
